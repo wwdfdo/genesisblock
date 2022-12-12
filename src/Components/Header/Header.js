3b2182bae62f77621577e746b1d9a653f9e839a8
wwdfdo <wwdfdo@gmail.com>
2022-12-12 08:43:46 +0530

    header styles first commit

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,7 +1,30 @@
 import React from "react";
+import Logo from "../Logo/Logo";
+import { navbarMenuItems, loginavMenu } from "../../arrays/NavBarArray";
 
 function Header() {
-  return <div></div>;
+  return (
+    <div className="h-[60px] ">
+      <div className="flex justify-between px-[15%] items-center border-b-[1px] border-b-[#008fb4] h-[100%]">
+        <div className="flex  items-center h-[100%] border-r-[1px] border-r-[#008fb4]">
+          <Logo />
+        </div>
+        <div className=" flex justify-between flex-1 px-6 font-semibold text-[0.875rem]">
+          <ul className="flex gap-6 items-center">
+            {navbarMenuItems.map((navmenuItem) => (
+              <li className={`${navmenuItem.styles}`}>{navmenuItem.title}</li>
+            ))}
+          </ul>
+
+          <ul className="flex items-center gap-6">
+            {loginavMenu.map((loginavItem) => (
+              <li className={`${loginavItem.styles}`}>{loginavItem.title}</li>
+            ))}
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Header;
---
+++ src/arrays/NavBarArray.js
@@ -0,0 +1,35 @@
+export const navbarMenuItems = [
+  {
+    id: 1,
+    title: "Post a task",
+    styles: "bg-[#008fb4] text-white px-4 py-1 rounded-3xl ",
+  },
+  {
+    id: 2,
+    title: "Catogories",
+  },
+  {
+    id: 3,
+    title: "Browse Tasks",
+  },
+  {
+    id: 4,
+    title: "How it works",
+  },
+];
+
+export const loginavMenu = [
+  {
+    id: 1,
+    title: "Sign In",
+  },
+  {
+    id: 2,
+    title: "Sign Up",
+  },
+  {
+    id: 3,
+    title: "Become a Tasker",
+    styles: "text-[#008fb4] px-4 py-1 rounded-3xl border-[#008fb4] border-2 ",
+  },
+];
