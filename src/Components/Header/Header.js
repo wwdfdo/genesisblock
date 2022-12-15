caa54c827d4661d3186a25319f47eeb9090e1d01
wwdfdo <wwdfdo@gmail.com>
2022-12-15 14:10:33 +0530

    added new navbar hoverstyles

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -9,16 +9,32 @@ function Header() {
         <div className="flex  items-center h-[100%] border-r-[0.02rem] border-opacity-25 border-r-[#008fb4] ">
           <Logo />
         </div>
-        <div className=" flex justify-between flex-1 px-6 font-semibold text-[0.825rem]">
-          <ul className="flex gap-6 items-center">
+        <div className=" flex justify-between flex-1 px-6 font-semibold text-[0.825rem] h-full items-stretch">
+          <ul className="flex gap-6 items-center h-full">
             {navbarMenuItems.map((navmenuItem) => (
-              <li className={`${navmenuItem.styles}`}>{navmenuItem.title}</li>
+              <li
+                className={`${navmenuItem.hovertextstyles} cursor-pointer flex items-center h-full`}
+              >
+                <span
+                  className={`${navmenuItem.styles} ${navmenuItem.textstyles} ${navmenuItem.hoverstyles} relative`}
+                >
+                  {navmenuItem.title}
+                </span>
+              </li>
             ))}
           </ul>
 
           <ul className="flex items-center gap-6">
             {loginavMenu.map((loginavItem) => (
-              <li className={`${loginavItem.styles}`}>{loginavItem.title}</li>
+              <li
+                className={`${loginavItem.hovertextstyles} cursor-pointer h-full items-center flex`}
+              >
+                <span
+                  className={`${loginavItem.styles} ${loginavItem.textstyles} ${loginavItem.hoverstyles} relative`}
+                >
+                  {loginavItem.title}
+                </span>
+              </li>
             ))}
           </ul>
         </div>
--- src/arrays/NavBarArray.js
+++ src/arrays/NavBarArray.js
@@ -2,19 +2,29 @@ export const navbarMenuItems = [
   {
     id: 1,
     title: "Post a task",
-    styles: "bg-[#008fb4] text-white px-4 py-1 rounded-3xl ",
+    styles:
+      "bg-[#008fb4] bg-opacity-80 hover:bg-opacity-100 text-white px-4 py-1 rounded-3xl  ",
   },
   {
     id: 2,
     title: "Catogories",
+    hovertextstyles: " hover:text-[#008fb4]",
+    textstyles: "h-full flex items-center",
+    hoverstyles: `before:w-full before:bg-[#008fb4] before:block before:absolute before:top-0 before:h-[3px] before:opacity-100 before:translate-y-0 before:opacity-0 hover:before:opacity-100 hover:before:transition hover:before:delay-200 hover:before:duration-300  active:before:opacity-100`,
   },
   {
     id: 3,
     title: "Browse Tasks",
+    hovertextstyles: " hover:text-[#008fb4]",
+    textstyles: "h-full flex items-center",
+    hoverstyles: `before:w-full before:bg-[#008fb4] before:block before:absolute before:top-0 before:h-[3px] before:opacity-100 before:translate-y-0 before:opacity-0 hover:before:opacity-100 hover:before:transition hover:before:delay-200 hover:before:duration-300`,
   },
   {
     id: 4,
     title: "How it works",
+    hovertextstyles: " hover:text-[#008fb4]",
+    textstyles: "h-full flex items-center",
+    hoverstyles: `before:w-full before:bg-[#008fb4] before:block before:absolute before:top-0 before:h-[3px] before:opacity-100 before:translate-y-0 before:opacity-0 hover:before:opacity-100 hover:before:transition hover:before:delay-200 hover:before:duration-300`,
   },
 ];
 
@@ -22,10 +32,16 @@ export const loginavMenu = [
   {
     id: 1,
     title: "Sign In",
+    hovertextstyles: " hover:text-[#008fb4]",
+    textstyles: "h-full flex items-center",
+    hoverstyles: `before:w-full before:bg-[#008fb4] before:block before:absolute before:top-0 before:h-[3px] before:opacity-100 before:translate-y-0 before:opacity-0 hover:before:opacity-100 hover:before:transition hover:before:delay-200 hover:before:duration-300`,
   },
   {
     id: 2,
     title: "Sign Up",
+    hovertextstyles: " hover:text-[#008fb4]",
+    textstyles: "h-full flex items-center",
+    hoverstyles: `before:w-full before:bg-[#008fb4] before:block before:absolute before:top-0 before:h-[3px] before:opacity-100 before:translate-y-0 before:opacity-0 hover:before:opacity-100 hover:before:transition hover:before:delay-200 hover:before:duration-300`,
   },
   {
     id: 3,
